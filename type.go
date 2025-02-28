aac7c96f0553eaa885f89e390121f7a8f8ddd5dd
zhangluo <zhangluo@zhanbgluo-MacBook-Pro.local>
2025-02-28 15:32:02 +0800

    debug:mini oss server 7

--- type.go
+++ type.go
@@ -11,12 +11,11 @@ type SuccessResponse struct {
 
 type SaveOSSRequest struct {
 	File_name_list []string `json:"file_name_list" binding:"required"`
-	Client_id      string   `json:"client_id" binding:"required"`
 	Ai_server_host string   `json:"ai_server_host" binding:"required"`
 	Ai_server_port string   `json:"ai_server_port" binding:"required"`
 }
 
 type OSSFile struct {
 	Filename string `json:"filename"`
-	OSS      string `json:"oss"`
+	OSS      string `json:"oss_url"`
 }
